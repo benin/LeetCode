48b6fd319368b3116c7ec5c18e92d5033de4261f
Volodymyr Karlov <Volodymyr_Karlov@epam.com>
2019-01-29 18:48:26 +0200

    Implemented simple solution

--- TwoSum/TwoSum.UnitTests/SolutionTests.cs
+++ TwoSum/TwoSum.UnitTests/SolutionTests.cs
@@ -9,17 +9,12 @@ namespace TwoSum.UnitTests
         {
         }
 
-        [Test]
-        public void ExampleTest()
+        [TestCase( new [] {2, 7, 11, 15}, 9, ExpectedResult=new[] {0, 1})]
+        [TestCase( new [] {3, 2, 4}, 6, ExpectedResult=new[] {1, 2})]
+        [TestCase( new [] {-1,-2,-3,-4,-5}, -8, ExpectedResult=new[] {2, 4})]
+        public int[] ExampleTest(int[] nums, int target)
         {
-            var nums = new [] {2, 7, 11, 15};
-            int target = 9;
-            
-            var solution = new Solution();
-
-            solution.TwoSum(nums, target);
-
-            Assert.Pass();
+            return new Solution().TwoSum(nums, target);            
         }
     }
 }
