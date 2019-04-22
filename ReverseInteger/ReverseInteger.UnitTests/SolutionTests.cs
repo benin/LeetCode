645b4d83f02d6db47d39019f827eab657a2fc1a4
Volodymyr Karlov <Volodymyr_Karlov@epam.com>
2019-04-22 16:16:44 +0300

    ReverseInteger: implemented 100% fastest solution with checked overflow test

---
+++ ReverseInteger/ReverseInteger.UnitTests/SolutionTests.cs
@@ -0,0 +1,37 @@
+using NUnit.Framework;
+using System.Collections;
+
+namespace ReverseInteger.UnitTests
+{
+    public class SolutionTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test, TestCaseSource(typeof(SolutionTestCases), "TestCases")]
+        public int SolutionTest(int x)
+        {
+            return new Solution().Reverse(x);
+        }
+    }
+
+    public class SolutionTestCases
+    {
+        public static IEnumerable TestCases
+        {
+            get
+            {
+                yield return new TestCaseData(123).Returns(321);
+                yield return new TestCaseData(-123).Returns(-321);
+                yield return new TestCaseData(120).Returns(21);
+                yield return new TestCaseData(1534236469).Returns(0);
+
+                yield return new TestCaseData(0).Returns(0);
+                yield return new TestCaseData(1).Returns(1);
+                yield return new TestCaseData(99).Returns(99);
+            }
+        }
+    }
+}
---
+++ ReverseInteger/ReverseInteger/Solution.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ReverseInteger
+{
+    public class Solution
+    {
+        public int Reverse(int x)
+        {
+            int res = 0;
+
+            try
+            {
+                while (x != 0)
+                {
+                    int part = x % 10;
+                    x /= 10;
+                    checked
+                    {
+                        res *= 10;
+                        res += part;
+                    }
+                }
+            } catch (System.OverflowException e)
+            {
+                res = 0;
+            }
+
+            return res;
+        }
+    }
+}
