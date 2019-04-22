a587fb723653e28367d5b6e5d30be5ae5c80b1ca
Volodymyr Karlov <Volodymyr_Karlov@epam.com>
2019-04-22 15:49:49 +0300

    OddEvenLinkedList:Implemented fastest solution

---
+++ OddEvenLinkedList/OddEvenLinkedList.UnitTests/SolutionTests.cs
@@ -0,0 +1,42 @@
+using NUnit.Framework;
+using System.Collections;
+
+namespace OddEvenLinkedList.UnitTests
+{
+    public class SolutionTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test, TestCaseSource(typeof(SolutionTestCases), "TestCases")]
+        public int[] SolutionTest(ListNode list)
+        {
+            new Solution().OddEvenList(list);
+            return list.ToArray();
+        }
+    }
+
+    public static class SolutionTestCases
+    {
+        public static IEnumerable TestCases
+        {
+            get
+            {
+                yield return new TestCaseData(new ListNode(new[] { 1, 2, 3, 4, 5 }))
+                    .Returns(new[] { 1, 3, 5, 2, 4 }).SetName("LeetCodeExample1");
+                yield return new TestCaseData(new ListNode(new[] { 2, 1, 3, 5, 6, 4, 7 }))
+                    .Returns(new[] { 2, 3, 6, 7, 1, 5, 4 }).SetName("LeetCodeExample2");
+                yield return new TestCaseData(new ListNode(new[] { 1, 2, 3, 4, 5, 6, 7, 8 }))
+                    .Returns(new []{1,3,5,7,2,4,6,8}).SetName("LeetCodeTestCase2");
+                yield return new TestCaseData(new ListNode(new[] { 1 }))
+                    .Returns(new[] { 1 }).SetName("One item");
+                yield return new TestCaseData(new ListNode(new[] { 1, 2 }))
+                    .Returns(new[] { 1, 2 }).SetName("No change");
+                yield return new TestCaseData(new ListNode(new[] { 1, 2, 3 }))
+                    .Returns(new[] { 1, 3, 2 }).SetName("123");
+            }
+        }
+    }
+}
---
+++ OddEvenLinkedList/OddEvenLinkedList/Solution.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace OddEvenLinkedList
+{
+    
+    // Definition for singly-linked list.
+    public class ListNode 
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x) 
+        { 
+            val = x; 
+        }
+
+        public ListNode(int[] nums)
+        {
+            if (nums == null || nums.Length == 0)
+                return;
+
+            val = nums[0];
+
+            var node = this;
+            for (int i = 1; i < nums.Length; i++)
+            {
+                node.next = new ListNode(nums[i]);
+                node = node.next;
+            }
+        }
+
+    }
+    
+    public class Solution
+    {
+        public ListNode OddEvenList(ListNode head) 
+        {
+            if (head?.next?.next == null)
+                return head;
+
+            var oddStartNode = head.next;
+            
+            var iter = head;
+            while(iter.next?.next != null)
+            {
+                var nextOdd = iter.next;
+                iter.next = iter.next.next;
+                nextOdd.next = iter.next.next;
+                iter = iter.next;
+            }
+
+            iter.next = oddStartNode;
+
+            return head;
+        }
+    }
+}
