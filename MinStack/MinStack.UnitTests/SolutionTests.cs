d83442e6c499e32bc964017089328b2152bc6cd2
Volodymyr Karlov <Volodymyr_Karlov@epam.com>
2019-04-24 17:13:29 +0300

    MinStack: implemented slow right solution

---
+++ MinStack/MinStack.UnitTests/SolutionTests.cs
@@ -0,0 +1,28 @@
+using NUnit.Framework;
+using FluentAssertions;
+
+namespace MinStack.UnitTests
+{
+    public class SolutionTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void MinStackLeetCodeExampleTest()
+        {
+            var minStack = new MinStack();
+
+            minStack.Push(-2);
+            minStack.Push(0);
+            minStack.Push(-3);
+
+            minStack.GetMin().Should().Be(-3);
+            minStack.Pop();
+            minStack.Top().Should().Be(0);
+            minStack.GetMin().Should().Be(-2);
+        }
+    }
+}
---
+++ MinStack/MinStack/MinStack.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MinStack
+{
+    /**
+     * Your MinStack object will be instantiated and called as such:
+     * MinStack obj = new MinStack();
+     * obj.Push(x);
+     * obj.Pop();
+     * int param_3 = obj.Top();
+     * int param_4 = obj.GetMin();
+     */
+    public class MinStack
+    {
+        private readonly LinkedList<LinkedListNode<int>> _stack = new LinkedList<LinkedListNode<int>>();
+        private readonly LinkedList<int> _minList = new LinkedList<int>();
+
+        public MinStack()
+        {
+        }
+
+        public void Push(int x)
+        {
+            LinkedListNode<int> newMinNode;
+
+            // Add minimal sorted list
+            if (_minList.First == null)
+            {
+                newMinNode = _minList.AddFirst(x);
+            }
+            else
+            {
+
+                var node = _minList.First;
+                while (node.Next != null && x > node.Value)
+                {
+                    node = node.Next;
+                }
+                newMinNode = x > node.Value ? 
+                    _minList.AddAfter(node, x) : 
+                    _minList.AddBefore(node, x);
+            }
+
+            // Add to the stack
+            _stack.AddLast(newMinNode);
+        }
+
+        public void Pop()
+        {
+            var removedNode = _stack.Last.Value;
+            _stack.RemoveLast();
+
+            _minList.Remove(removedNode);
+        }
+
+        public int Top()
+        {
+            return _stack.Last.Value.Value;
+        }
+
+        public int GetMin()
+        {
+            return _minList.First.Value;
+        }
+    }
+}
